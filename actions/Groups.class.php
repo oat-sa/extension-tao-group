b3845012bee8cce66d552f72cb563a42d59b5197
bchevrier <bchevrier@62dfeb86-a93f-4914-9d13-15c0a160985d>
2009-10-29 16:16:56 +0000

    integrate structure, update model
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoGroups/trunk@1602 62dfeb86-a93f-4914-9d13-15c0a160985d

--- actions/Groups.class.php
+++ actions/Groups.class.php
@@ -2,10 +2,90 @@
 
 class Groups extends Module {
 
+	protected $service = null;
+
+	public function __construct(){
+		$this->service = tao_models_classes_ServiceFactory::get('Groups');
+	}
+	
+/*
+ * controller actions
+ */
+
+	/**
+	 * main action
+	 * @return void
+	 */
 	public function index(){
-		$this->setData('testVar', "this is the ". get_class($this) ." module");
+		$context = Context::getInstance();
+		$this->setData('content', "this is the ". get_class($this) ." module, " . $context->getActionName());
+		$this->setView('index.tpl');
+	}
+	
+		/**
+	 * Render json data to populate the subject tree 
+	 * 'modelType' must be in request parameter
+	 * @return void
+	 */
+	public function getGroups(){
 		
-		$this->setView('view.tpl');
+		if(!tao_helpers_Request::isAjax()){
+			throw new Exception("wrong request mode");
+		}
+		$highlightUri = '';
+		if($this->hasSessionAttribute("showNodeUri")){
+			$highlightUri = $this->getSessionAttribute("showNodeUri");
+			unset($_SESSION[SESSION_NAMESPACE]["showNodeUri"]);
+		} 
+		echo json_encode( $this->service->toTree( $this->service->getGroupClass(), true, true, $highlightUri));
+	}
+	
+	/*
+	 * TODO
+	 */
+	 public function import(){
+		$context = Context::getInstance();
+		$this->setData('content', "this is the ". get_class($this) ." module, " . $context->getActionName());
+		$this->setView('index.tpl');
+	}
+	
+	 public function export(){
+		$context = Context::getInstance();
+		$this->setData('content', "this is the ". get_class($this) ." module, " . $context->getActionName());
+		$this->setView('index.tpl');
+	}
+	
+		/*
+	 * conveniance methods
+	 */
+	
+	private function getCurrentGroup(){
+		$uri = tao_helpers_Uri::decode($this->getRequestParameter('uri'));
+		if(is_null($uri) || empty($uri)){
+			throw new Exception("No valid uri found");
+		}
+		
+		$model = $this->getCurrentModel();
+		$group = $this->service->getGroup($uri, 'uri', $model);
+		if(is_null($group)){
+			throw new Exception("No group found for the uri {$uri}");
+		}
+		
+		return $group;
+	}
+	
+	private function getCurrentModel(){
+		$classUri = tao_helpers_Uri::decode($this->getRequestParameter('classUri'));
+		if(is_null($classUri) || empty($classUri)){
+			throw new Exception("No valid uri found");
+		}
+		
+		$model = new core_kernel_classes_Class($classUri);
+		if(is_null($model)){
+			throw new Exception("No class found for the uri {$classUri}");
+		}
+		
+		return $model;
 	}
 }
 ?>
--- models/classes/class.GroupService.php
+++ models/classes/class.GroupService.php
@@ -3,16 +3,16 @@
 error_reporting(E_ALL);
 
 /**
- * Generis Object Oriented API - taoGroups\models\classes\class.GroupService.php
+ * Generis Object Oriented API - taoGroups/models/classes/class.GroupService.php
  *
  * $Id$
  *
  * This file is part of Generis Object Oriented API.
  *
- * Automatically generated on 14.09.2009, 15:14:24 with ArgoUML PHP module 
+ * Automatically generated on 29.10.2009, 13:59:15 with ArgoUML PHP module 
  * (last revised $Date: 2008-04-19 08:22:08 +0200 (Sat, 19 Apr 2008) $)
  *
- * @author Bertrand Chevrier, <taosupport@tudor.lu>
+ * @author Bertrand Chevrier, <chevrier.bertrand@gmail.com>
  * @package taoGroups
  * @subpackage models_classes
  */
@@ -25,7 +25,7 @@ if (0 > version_compare(PHP_VERSION, '5')) {
  * The Service class is an abstraction of each service instance. 
  * Used to centralize the behavior related to every servcie instances.
  *
- * @author Bertrand Chevrier, <taosupport@tudor.lu>
+ * @author Bertrand Chevrier, <chevrier.bertrand@gmail.com>
  */
 require_once('tao/models/classes/class.Service.php');
 
@@ -41,7 +41,7 @@ require_once('tao/models/classes/class.Service.php');
  * Short description of class taoGroups_models_classes_GroupService
  *
  * @access public
- * @author Bertrand Chevrier, <taosupport@tudor.lu>
+ * @author Bertrand Chevrier, <chevrier.bertrand@gmail.com>
  * @package taoGroups
  * @subpackage models_classes
  */
@@ -53,13 +53,104 @@ class taoGroups_models_classes_GroupService
 
     // --- ATTRIBUTES ---
 
+    /**
+     * Short description of attribute groupClass
+     *
+     * @access protected
+     * @var Class
+     */
+    protected $groupClass = null;
+
+    /**
+     * Short description of attribute groupsOntologies
+     *
+     * @access protected
+     * @var array
+     */
+    protected $groupsOntologies = array('http://www.tao.lu/Ontologies/TAOGroup.rdf');
+
     // --- OPERATIONS ---
+
+    /**
+     * Short description of method __construct
+     *
+     * @access public
+     * @author Bertrand Chevrier, <chevrier.bertrand@gmail.com>
+     * @return mixed
+     */
+    public function __construct()
+    {
+        // section 127-0-1-1-506607cb:1249f78eef0:-8000:0000000000001AEB begin
+		
+		parent::__construct();
+		$this->groupClass = new core_kernel_classes_Class(TAO_GROUP_CLASS);
+		$this->loadOntologies($this->groupsOntologies);
+		
+        // section 127-0-1-1-506607cb:1249f78eef0:-8000:0000000000001AEB end
+    }
+
+    /**
+     * Short description of method getGroupClass
+     *
+     * @access public
+     * @author Bertrand Chevrier, <chevrier.bertrand@gmail.com>
+     * @param  string uri
+     * @return core_kernel_classes_Class
+     */
+    public function getGroupClass($uri = '')
+    {
+        $returnValue = null;
+
+        // section 127-0-1-1--5cd530d7:1249feedb80:-8000:0000000000001AE8 begin
+		
+		$returnValue = $this->groupClass;
+		
+        // section 127-0-1-1--5cd530d7:1249feedb80:-8000:0000000000001AE8 end
+
+        return $returnValue;
+    }
+
+    /**
+     * Short description of method getGroup
+     *
+     * @access public
+     * @author Bertrand Chevrier, <chevrier.bertrand@gmail.com>
+     * @param  mixed identifier usually the test label or the ressource URI
+     * @return core_kernel_classes_Resource
+     */
+    public function getGroup( mixed $identifier)
+    {
+        $returnValue = null;
+
+        // section 10-13-1-45-792423e0:12398d13f24:-8000:00000000000017D5 begin
+        // section 10-13-1-45-792423e0:12398d13f24:-8000:00000000000017D5 end
+
+        return $returnValue;
+    }
+
+    /**
+     * Short description of method getGroups
+     *
+     * @access public
+     * @author Bertrand Chevrier, <chevrier.bertrand@gmail.com>
+     * @param  array options
+     * @return core_kernel_classes_ContainerCollection
+     */
+    public function getGroups($options)
+    {
+        $returnValue = null;
+
+        // section 10-13-1-45-792423e0:12398d13f24:-8000:0000000000001809 begin
+        // section 10-13-1-45-792423e0:12398d13f24:-8000:0000000000001809 end
+
+        return $returnValue;
+    }
 
     /**
      * Short description of method createGroup
      *
      * @access public
-     * @author Bertrand Chevrier, <taosupport@tudor.lu>
+     * @author Bertrand Chevrier, <chevrier.bertrand@gmail.com>
      * @param  string label
      * @param  ContainerCollection members
      * @param  ContainerCollection tests
@@ -76,46 +167,10 @@ class taoGroups_models_classes_GroupService
     }
 
     /**
-     * Short description of method getGroups
-     *
-     * @access public
-     * @author Bertrand Chevrier, <taosupport@tudor.lu>
-     * @param  array options
-     * @return core_kernel_classes_ContainerCollection
-     */
-    public function getGroups($options)
-    {
-        $returnValue = null;
-
-        // section 10-13-1-45-792423e0:12398d13f24:-8000:0000000000001809 begin
-        // section 10-13-1-45-792423e0:12398d13f24:-8000:0000000000001809 end
-
-        return $returnValue;
-    }
-
-    /**
-     * Short description of method getGroup
-     *
-     * @access public
-     * @author Bertrand Chevrier, <taosupport@tudor.lu>
-     * @param  mixed identifier usually the test label or the ressource URI
-     * @return core_kernel_classes_Resource
-     */
-    public function getGroup( mixed $identifier)
-    {
-        $returnValue = null;
-
-        // section 10-13-1-45-792423e0:12398d13f24:-8000:00000000000017D5 begin
-        // section 10-13-1-45-792423e0:12398d13f24:-8000:00000000000017D5 end
-
-        return $returnValue;
-    }
-
-    /**
      * Short description of method deleteGroup
      *
      * @access public
-     * @author Bertrand Chevrier, <taosupport@tudor.lu>
+     * @author Bertrand Chevrier, <chevrier.bertrand@gmail.com>
      * @param  Resource group
      * @return boolean
      */
@@ -125,6 +180,24 @@ class taoGroups_models_classes_GroupService
 
         // section 10-13-1-45-792423e0:12398d13f24:-8000:0000000000001806 begin
         // section 10-13-1-45-792423e0:12398d13f24:-8000:0000000000001806 end
+
+        return (bool) $returnValue;
+    }
+
+    /**
+     * Short description of method isGroupSubClass
+     *
+     * @access public
+     * @author Bertrand Chevrier, <chevrier.bertrand@gmail.com>
+     * @param  Class clazz
+     * @return boolean
+     */
+    public function isGroupSubClass( core_kernel_classes_Class $clazz)
+    {
+        $returnValue = (bool) false;
+
+        // section 127-0-1-1--5cd530d7:1249feedb80:-8000:0000000000001AEA begin
+        // section 127-0-1-1--5cd530d7:1249feedb80:-8000:0000000000001AEA end
 
         return (bool) $returnValue;
     }
