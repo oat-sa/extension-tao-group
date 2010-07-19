1d97b9f093f8039ad15781fb4b80c51e15ebfdc5
bchevrier <bchevrier@62dfeb86-a93f-4914-9d13-15c0a160985d>
2010-07-19 16:14:32 +0000

    * update services mode context
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoGroups/trunk@2987 62dfeb86-a93f-4914-9d13-15c0a160985d

--- actions/SaSGroups.class.php
+++ actions/SaSGroups.class.php
@@ -13,6 +13,7 @@ class SaSGroups extends Groups {
      * @see Groups::__construct()
      */
     public function __construct() {
+    	tao_helpers_Context::load('STANDALONE_MODE');
         $this->setSessionAttribute('currentExtension', 'taoGroups');
 		parent::__construct();
     }
