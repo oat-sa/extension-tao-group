73f3bb7a8d98652d9821be78ab55a1a7d807d195
jbogaerts <jbogaerts@62dfeb86-a93f-4914-9d13-15c0a160985d>
2011-07-26 12:00:54 +0000

    PO Update
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoGroups/trunk@6507 62dfeb86-a93f-4914-9d13-15c0a160985d

---
+++ locales/_raw/messages_po.js
@@ -0,0 +1,4 @@
+/* auto generated content */
+/* extesion: taoGroups, lang: _raw */
+var langCode = '_raw';
+var i18n_tr = {"Class saved":"","Edit group class":"","Group saved":"","Groups":"","Groups library":"","Manage groups":"","Related tests":"","Save":"","Select group deliveries":"","Select group test takers":"","The Group module allows to groupe Test takers according to global features and classifications.":"","The main objective of the Group module is to assign the test(s) to the appointed group, thus establishing the basis for running a test.":"","create table":"","delete":"","duplicate":"","export":"","import":"","lists":"","meta data":"","move":"","new class":"","new group":"","search":""};
