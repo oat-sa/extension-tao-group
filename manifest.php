9a4e13da42d5e30f6e979d6b9555a1ec2830a61d
jbout <jbout@62dfeb86-a93f-4914-9d13-15c0a160985d>
2012-06-15 12:19:16 +0000

    updated extension nrs to 2.3 removed force smooth for versions pre 2.1
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoGroups/trunk@11488 62dfeb86-a93f-4914-9d13-15c0a160985d

--- manifest.php
+++ manifest.php
@@ -8,7 +8,7 @@ return array(
 	'name' => 'taoGroups',
 	'description' => 'TAO Groups extension',
 	'additional' => array(
-		'version' => '2.0',
+		'version' => '2.3',
 		'author' => 'CRP Henri Tudor',
 		'extends' => 'tao',
 		'dependances' => array('tao'),
