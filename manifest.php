f9f80a203e256a3abda24f2862808070dcde9e20
jbout <jbout@62dfeb86-a93f-4914-9d13-15c0a160985d>
2012-09-17 16:08:01 +0000

    * moved extension system constants from config to manifest * flatened structure of manifest * allow access to constants of specific extensions * removed unused constants
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoGroups/trunk@12116 62dfeb86-a93f-4914-9d13-15c0a160985d

--- manifest.php
+++ manifest.php
@@ -4,25 +4,59 @@
  * @license GPLv2  http://www.opensource.org/licenses/gpl-2.0.php
  *
  */
+$extpath = dirname(__FILE__).DIRECTORY_SEPARATOR;
+$taopath = dirname(dirname(__FILE__)).DIRECTORY_SEPARATOR.'tao'.DIRECTORY_SEPARATOR;
+
 return array(
 	'name' => 'taoGroups',
 	'description' => 'TAO Groups extension',
-	'additional' => array(
-		'version' => '2.3',
-		'author' => 'CRP Henri Tudor',
-		'extends' => 'tao',
-		'dependances' => array('taoSubjects'),
-		'models' => array('http://www.tao.lu/Ontologies/TAOGroup.rdf',
-			'http://www.tao.lu/Ontologies/taoFuncACL.rdf'),
-		'install' => array(
-			'rdf' => array(
-					array('ns' => 'http://www.tao.lu/Ontologies/TAOGroup.rdf', 'file' => dirname(__FILE__). '/models/ontology/taogroup.rdf'),
-			)
+	'version' => '2.3',
+	'author' => 'CRP Henri Tudor',
+	'dependances' => array('taoSubjects'),
+	'models' => array(
+		'http://www.tao.lu/Ontologies/TAOGroup.rdf',
+		'http://www.tao.lu/Ontologies/taoFuncACL.rdf'
+	),
+	'install' => array(
+		'rdf' => array(
+				array('ns' => 'http://www.tao.lu/Ontologies/TAOGroup.rdf', 'file' => dirname(__FILE__). '/models/ontology/taogroup.rdf'),
 		),
-		'classLoaderPackages' => array(
-			dirname(__FILE__).'/actions/',
-			dirname(__FILE__).'/helpers/'
-		 )
+	),
+	'classLoaderPackages' => array(
+		dirname(__FILE__).'/actions/',
+		dirname(__FILE__).'/helpers/'
+	 ),
+	 'constants' => array(
+		 "DIR_ACTIONS"			=> $extpath."actions".DIRECTORY_SEPARATOR,
+	
+		# models directory
+		"DIR_MODELS"			=> $extpath."models".DIRECTORY_SEPARATOR,
+	
+		# views directory
+		"DIR_VIEWS"				=> $extpath."views".DIRECTORY_SEPARATOR,
+	
+		# helpers directory
+		"DIR_HELPERS"			=> $extpath."helpers".DIRECTORY_SEPARATOR,
+	
+		# default module name
+		'DEFAULT_MODULE_NAME'	=> 'Groups',
+	
+		#default action name
+		'DEFAULT_ACTION_NAME'	=> 'index',
+	
+		#BASE PATH: the root path in the file system (usually the document root)
+		'BASE_PATH'				=> $extpath . 'taoGroups',
+	
+		#BASE URL (usually the domain root)
+		'BASE_URL'				=> ROOT_URL . '/taoGroups',
+	
+		#BASE WWW the web resources path
+		'BASE_WWW'				=> ROOT_URL . '/taoGroups/views/',
+	 
+	 	#TAO extension Paths
+		'TAOBASE_WWW'			=> ROOT_URL  . '/tao/views/',
+		'TAOVIEW_PATH'			=> $taopath.'views'.DIRECTORY_SEPARATOR,
+		'TAO_TPL_PATH'			=> $taopath.'views'.DIRECTORY_SEPARATOR.'templates'.DIRECTORY_SEPARATOR,
 	)
 );
 ?>
