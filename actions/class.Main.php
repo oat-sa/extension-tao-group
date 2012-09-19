d823022bd42f9f7101a778f520558aafcc9d1710
jbout <jbout@62dfeb86-a93f-4914-9d13-15c0a160985d>
2012-09-19 14:08:07 +0000

    * redirected getTree/getActions to extension for translation * fixed manifests
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoGroups/trunk@12162 62dfeb86-a93f-4914-9d13-15c0a160985d

---
+++ actions/class.Main.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package taoGroups
+ * @subpackage actions
+ * @license GPLv2  http://www.opensource.org/licenses/gpl-2.0.php
+ * 
+ */
+
+class taoGroups_actions_Main extends tao_actions_Main {
+	
+}
+?>
