d75c679f8440442da8ab3369bc5ab5a2ff40cd26
jbout <jbout@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-04-09 07:23:33 +0000

    removed unused constant
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoGroups/trunk@19550 62dfeb86-a93f-4914-9d13-15c0a160985d

--- includes/constants.php
+++ includes/constants.php
@@ -1,5 +1,5 @@
 <?php
-/*  
+/**  
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; under version 2
@@ -19,15 +19,12 @@
  *               2009-2012 (update and modification) Public Research Centre Henri Tudor (under the project TAO-SUSTAIN & TAO-DEV);
  * 
  */
-?>
-<?php
-/*
+
+/**
  * @author CRP Henri Tudor - TAO Team - {@link http://www.tao.lu}
  * @license GPLv2  http://www.opensource.org/licenses/gpl-2.0.php
  * 
  */
 $todefine = array(
-	'TAO_GROUP_MEMBERS_PROP'=> 'http://www.tao.lu/Ontologies/TAOGroup.rdf#Members',
-	'TAO_GROUP_DELIVERIES_PROP'	=> 'http://www.tao.lu/Ontologies/TAOGroup.rdf#Deliveries'
+	'TAO_GROUP_MEMBERS_PROP'=> 'http://www.tao.lu/Ontologies/TAOGroup.rdf#Members'
 );
-?>
