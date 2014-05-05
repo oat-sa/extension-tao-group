19925543a0194349603ecfd0a659c048774a495e
pplichart <pplichart@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-05-05 15:16:00 +0000

    changed description (ran taoTranslate.php -v -a compile -l en-US -e taoGroups and update )
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoGroups/trunk@20063 62dfeb86-a93f-4914-9d13-15c0a160985d

--- locales/en-US/messages_po.js
+++ locales/en-US/messages_po.js
@@ -3,6 +3,6 @@
 define(function(){
  return {
    langCode : 'en-US',
-   i18n_tr : []
+   i18n_tr : {"The Groups module allows to group Test takers according to global features and classifications.":"The Groups module allows you to segment test-takers based on specific criteria, and to easily assign tests to entire groups.\u0000"}
  };
 });
