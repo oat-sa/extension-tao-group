158589a7ec49ccbc25a5c0b110d71a7a60b2162e
Lionel Lecaque <lionel@taotesting.com>
2014-10-14 13:21:04 +0200

    update locales

--- locales/en-US/messages_po.js
+++ locales/en-US/messages_po.js
@@ -1,1 +1,1 @@
-{"Group test takers according to global features and classifications.":"Group test-takers according to global features and classifications.","Manage test takers":"Manage test-takers","Record and manage test takers.":"Record and manage test-takers.","Select group test takers":"Select group test-takers","The Groups module allows to group Test takers according to global features and classifications.":"The Groups module allows you to segment test-takers based on specific criteria, and to easily assign tests to entire groups.\u0000"}
+[]
