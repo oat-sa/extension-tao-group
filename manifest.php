ab5cd3c821cbf4e75370e2eed1204b327a703f0a
antoinerobin <antoine.robin@vesperiagroup.com>
2016-10-07 16:47:01 +0200

    bump version number

--- manifest.php
+++ manifest.php
@@ -34,7 +34,7 @@ return array(
     'label' => 'Groups core extension',
 	'description' => 'TAO Groups extension',
     'license' => 'GPL-2.0',
-    'version' => '2.10.1',
+    'version' => '2.10.2',
 	'author' => 'Open Assessment Technologies, CRP Henri Tudor',
 	'requires' => array(
 	    'taoTestTaker' => '>=2.6',
--- models/update/Updater.php
+++ models/update/Updater.php
@@ -65,7 +65,6 @@ class Updater extends \common_ext_ExtensionUpdater
             OntologyUpdater::syncModels();
             $this->setVersion('2.7.1');
         }
-        $this->skip('2.7.1','2.10.1');
-        return null;
+        $this->skip('2.7.1','2.10.2');
     }
 }
