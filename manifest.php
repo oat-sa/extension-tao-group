38c514b195c046e112d8fc0d39fa7043a6f5d62c
Aleksej Tikhanovich <aleksej@taotesting.com>
2017-12-19 16:02:53 +0300

    Bump version

--- manifest.php
+++ manifest.php
@@ -34,12 +34,12 @@ return array(
     'label' => 'Groups core extension',
 	'description' => 'TAO Groups extension',
     'license' => 'GPL-2.0',
-    'version' => '3.1.0',
+    'version' => '3.2.0',
 	'author' => 'Open Assessment Technologies, CRP Henri Tudor',
 	'requires' => array(
 	    'taoTestTaker' => '>=2.6',
 	    'taoBackOffice' => '>=0.8',
-        'generis' => '>=5.9.0'
+        'generis' => '>=5.11.0'
     ),
 	'models' => array(
 		'http://www.tao.lu/Ontologies/TAOGroup.rdf'
