a08280c4c24c9828c1ecbfc28b4374660ae0e512
siarhei.baradzin <siarhei@taotesting.com>
2019-04-02 14:14:35 +0300

    bump version

--- manifest.php
+++ manifest.php
@@ -34,7 +34,7 @@ return array(
     'label' => 'Groups core extension',
     'description' => 'TAO Groups extension',
     'license' => 'GPL-2.0',
-    'version' => '5.0.0',
+    'version' => '5.0.1',
     'author' => 'Open Assessment Technologies, CRP Henri Tudor',
     'requires' => array(
         'taoTestTaker' => '>=4.0.0',
