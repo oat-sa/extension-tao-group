4c42183c36d4cb412b08fab4c86d0b3ecc1f1353
siwane <camille@taotesting.com>
2019-05-06 14:00:27 +0200

    Bump to version 4.2.1

--- manifest.php
+++ manifest.php
@@ -34,7 +34,7 @@ return array(
     'label' => 'Groups core extension',
     'description' => 'TAO Groups extension',
     'license' => 'GPL-2.0',
-    'version' => '4.2.0',
+    'version' => '4.2.1',
     'author' => 'Open Assessment Technologies, CRP Henri Tudor',
     'requires' => array(
         'taoTestTaker' => '>=4.0.0',
